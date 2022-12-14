87341e830d6306654c59cd39cb758874ec2ef2e0
bugi <bugi@joboland.de>
2022-12-14 18:46:28 +0100

    cargo fmt

--- src/day13/mod.rs
+++ src/day13/mod.rs
@@ -1,6 +1,6 @@
-use std::cmp::Ordering;
 use crate::day13::ListItem::{ItemList, Value};
 use crate::day13::ParseMode::{FindItems, FindRight};
+use std::cmp::Ordering;
 
 #[derive(Debug, Eq)]
 enum ListItem {
@@ -14,11 +14,10 @@ impl PartialEq for ListItem {
             (Value(a), Value(b)) => a == b,
             (Value(a), ItemList(b)) => vec![Value(*a)] == *b,
             (ItemList(a), Value(b)) => vec![Value(*b)] == *a,
-            (ItemList(a), ItemList(b)) => a == b
+            (ItemList(a), ItemList(b)) => a == b,
         }
     }
 }
-
 
 impl Ord for ListItem {
     fn cmp(&self, other: &Self) -> Ordering {
@@ -26,7 +25,7 @@ impl Ord for ListItem {
             (Value(a), Value(b)) => a.cmp(b),
             (Value(a), ItemList(b)) => vec![Value(*a)].cmp(b),
             (ItemList(a), Value(b)) => a.cmp(&vec![Value(*b)]),
-            (ItemList(a), ItemList(b)) => a.cmp(b)
+            (ItemList(a), ItemList(b)) => a.cmp(b),
         }
     }
 }
@@ -36,7 +35,6 @@ impl PartialOrd for ListItem {
         Some(self.cmp(other))
     }
 }
-
 
 enum ParseMode {
     FindRight,
@@ -57,86 +55,99 @@ fn parse_list(input: &str) -> ListItem {
 
     while cursor < stream.len() {
         match parse_mode {
-            FindRight => {
-                match stream[cursor] {
-                    '[' => {
-                        level += 1;
-                    }
-                    ']' => {
-                        level -= 1;
-                        if level == 0 {
-                            items.push(parse_list(&input[capture_start..cursor]));
-                            parse_mode = FindItems;
-                            capture_start = cursor + 1;
-                        }
-                    }
-                    _ => {}
+            FindRight => match stream[cursor] {
+                '[' => {
+                    level += 1;
                 }
-            }
-            FindItems => {
-                match stream[cursor] {
-                    '[' => {
-                        parse_mode = FindRight;
-                        capture_start = cursor + 1;
-                        level += 1;
-                    }
-                    ',' | ']' => {
-                        if cursor > capture_start {
-                            items.push(Value(stream[capture_start..cursor].iter().collect::<String>().parse().unwrap()));
-                        }
+                ']' => {
+                    level -= 1;
+                    if level == 0 {
+                        items.push(parse_list(&input[capture_start..cursor]));
+                        parse_mode = FindItems;
                         capture_start = cursor + 1;
                     }
-                    _ => {}
                 }
-            }
+                _ => {}
+            },
+            FindItems => match stream[cursor] {
+                '[' => {
+                    parse_mode = FindRight;
+                    capture_start = cursor + 1;
+                    level += 1;
+                }
+                ',' | ']' => {
+                    if cursor > capture_start {
+                        items.push(Value(
+                            stream[capture_start..cursor]
+                                .iter()
+                                .collect::<String>()
+                                .parse()
+                                .unwrap(),
+                        ));
+                    }
+                    capture_start = cursor + 1;
+                }
+                _ => {}
+            },
         }
 
         cursor += 1;
     }
     if cursor > capture_start {
-        items.push(Value(stream[capture_start..cursor].iter().collect::<String>().parse().unwrap()));
+        items.push(Value(
+            stream[capture_start..cursor]
+                .iter()
+                .collect::<String>()
+                .parse()
+                .unwrap(),
+        ));
     }
 
     ItemList(items)
 }
 
-
 pub fn run() {}
 
 pub fn run2() {}
 
-
 #[cfg(test)]
 mod test {
-    use crate::read_file;
     use super::*;
+    use crate::read_file;
 
     #[test]
     fn list_parsing() {
         assert_eq!(ItemList(vec![ItemList(vec![])]), parse_list("[]"));
         assert_eq!(ItemList(vec![ItemList(vec![Value(1)])]), parse_list("[1]"));
-        assert_eq!(ItemList(vec![ItemList(vec![Value(1), Value(2)])]), parse_list("[1,2]"));
-        assert_eq!(ItemList(vec![ItemList(vec![Value(1), ItemList(vec![Value(1), Value(2)])])]), parse_list("[1,[1,2]]"));
-        assert_eq!(ItemList(vec![
-            ItemList(
-                vec![
-                    Value(1),
+        assert_eq!(
+            ItemList(vec![ItemList(vec![Value(1), Value(2)])]),
+            parse_list("[1,2]")
+        );
+        assert_eq!(
+            ItemList(vec![ItemList(vec![
+                Value(1),
+                ItemList(vec![Value(1), Value(2)])
+            ])]),
+            parse_list("[1,[1,2]]")
+        );
+        assert_eq!(
+            ItemList(vec![ItemList(vec![
+                Value(1),
+                ItemList(vec![
+                    Value(2),
                     ItemList(vec![
-                        Value(2),
+                        Value(3),
                         ItemList(vec![
-                            Value(3),
-                            ItemList(vec![
-                                Value(4),
-                                ItemList(vec![
-                                    Value(5), Value(6), Value(0),
-                                ]),
-                            ]),
+                            Value(4),
+                            ItemList(vec![Value(5), Value(6), Value(0),]),
                         ]),
                     ]),
-                    Value(8),
-                    Value(9),
-                ])
-        ]), parse_list("[1,[2,[3,[4,[5,6,0]]]],8,9]"));
+                ]),
+                Value(8),
+                Value(9),
+            ])]),
+            parse_list("[1,[2,[3,[4,[5,6,0]]]],8,9]")
+        );
     }
 
     #[test]
@@ -186,7 +197,6 @@ mod test {
             .enumerate()
             .find(|(i, item)| ItemList(vec![ItemList(vec![Value(6)])]) == **item)
             .unwrap();
-
 
         println!("{:?}", { (marker1.0 + 1) * (marker2.0 + 1) });
     }
