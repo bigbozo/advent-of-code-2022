3c688b6ac6b5c6d9b9eae0b45311c084fb18d3f5
bugi <bugi@joboland.de>
2022-12-20 23:05:13 +0100

    day 19 - test part 1 working

--- src/day19/mod.rs
+++ src/day19/mod.rs
@@ -1,250 +1,248 @@
-use std::cmp::max;
-use std::collections::{HashMap};
-use regex::{Captures, Regex};
-use crate::day19::Material::{Clay, Geode, Obsidian, Ore};
 use crate::read_file;
+use rayon::prelude::*;
+use regex::{Captures, Regex};
+use std::cmp::max;
+use std::fmt::{Display, Formatter};
 
-#[derive(Eq, Hash, PartialEq, Debug)]
-enum Material {
-    Ore(u32),
-    Clay(u32),
-    Obsidian(u32),
-    Geode(u32),
+#[derive(Clone)]
+struct GameState {
+    // built robots
+    ore_robots: u32,
+    clay_robots: u32,
+    obsidian_robots: u32,
+    geode_robots: u32,
+    // blueprint_costs
+    ore_cost: u32,
+    clay_cost: u32,
+    obsidian_ore_cost: u32,
+    obsidian_clay_cost: u32,
+    geode_ore_cost: u32,
+    geode_obsidian_cost: u32,
+    // harvested items
+    ores: u32,
+    clays: u32,
+    obsidians: u32,
+    geodes: u32,
+    max_ore_robots: u32,
+    max_clay_robots: u32,
+    max_obsidian_robots: u32,
 }
 
-type RobotType = Vec<Material>;
-
-#[derive(Debug)]
-struct Blueprint {
-    ore_robot: RobotType,
-    clay_robot: RobotType,
-    obsidian_robot: RobotType,
-    geode_robot: RobotType,
+impl Display for GameState {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "[{},{},{},{} / {},{},{},{}]",
+            self.ores, self.clays, self.obsidians, self.geodes,
+            self.ore_robots, self.clay_robots, self.obsidian_robots, self.geode_robots
+        )
+    }
 }
 
-#[derive(Debug)]
-struct Robot {
-    produces: Material,
+impl GameState {
+    pub fn new(
+        ore_cost: u32,
+        clay_cost: u32,
+        obsidian_ore_cost: u32,
+        obsidian_clay_cost: u32,
+        geode_ore_cost: u32,
+        geode_obsidian_cost: u32,
+    ) -> GameState {
+        GameState {
+            ore_robots: 1,
+            clay_robots: 0,
+            obsidian_robots: 0,
+            geode_robots: 0,
+            // blueprint_costs
+            ore_cost,
+            clay_cost,
+            obsidian_ore_cost,
+            obsidian_clay_cost,
+            geode_ore_cost,
+            geode_obsidian_cost,
+            // harvested items
+            ores: 0,
+            clays: 0,
+            obsidians: 0,
+            geodes: 0,
+
+            max_ore_robots: max(max(ore_cost, obsidian_ore_cost), geode_ore_cost),
+            max_clay_robots: obsidian_clay_cost,
+            max_obsidian_robots: geode_obsidian_cost,
+        }
+    }
+
+    pub fn collect_materials(&mut self) {
+        self.ores += self.ore_robots;
+        self.clays += self.clay_robots;
+        self.obsidians += self.obsidian_robots;
+        self.geodes += self.geode_robots;
+    }
+
+    pub fn uncollect_materials(&mut self) {
+        self.ores -= self.ore_robots;
+        self.clays -= self.clay_robots;
+        self.obsidians -= self.obsidian_robots;
+        self.geodes -= self.geode_robots;
+    }
 }
 
-fn parse_input(input: String) -> Vec<Blueprint> {
-    let mut blueprints = vec![];
+fn parse_input(input: String) -> Vec<GameState> {
+    let mut game_state = vec![];
 
     for line in input.lines() {
-        blueprints.push(parse_line(line));
+        game_state.push(parse_line(line));
     }
 
-
-    blueprints
+    game_state
 }
 
-fn parse_line(line: &str) -> Blueprint {
+fn parse_line(line: &str) -> GameState {
     let regex = Regex::new(r"Blueprint \d+: Each ore robot costs (\d+) ore. Each clay robot costs (\d+) ore. Each obsidian robot costs (\d+) ore and (\d+) clay. Each geode robot costs (\d+) ore and (\d+) obsidian.").unwrap();
 
-    let cap: Captures = regex.captures(line)
-        .unwrap();
+    let cap: Captures = regex.captures(line).unwrap();
 
-    Blueprint {
-        ore_robot: vec![Ore(cap[1].parse().unwrap())],
-        clay_robot: vec![Ore(cap[2].parse().unwrap())],
-        obsidian_robot: vec![Ore(cap[3].parse().unwrap()), Clay(cap[4].parse().unwrap())],
-        geode_robot: vec![Ore(cap[5].parse().unwrap()), Obsidian((cap[6]).parse().unwrap())],
-    }
+    GameState::new(
+        cap[1].parse().unwrap(),
+        cap[2].parse().unwrap(),
+        cap[3].parse().unwrap(),
+        cap[4].parse().unwrap(),
+        cap[5].parse().unwrap(),
+        cap[6].parse().unwrap(),
+    )
 }
 
-
-pub fn run() {
-    let blueprints = parse_input(read_file("input/day19-test.txt"));
-    for blueprint in blueprints {
-        let mut robots = vec![Robot { produces: Ore(1) }];
-        let mut items = HashMap::new();
-
-        items.insert(Ore(1), 0);
-        items.insert(Clay(1), 0);
-        items.insert(Obsidian(1), 0);
-        items.insert(Geode(1), 0);
-
-        println!("SCore for Blueprint: {}", run_simulation(&blueprint, &mut items, &mut robots, 24));
-    }
-}
-
-fn run_simulation(blueprint: &Blueprint, items: &mut HashMap<Material, u32>, robots: &mut Vec<Robot>, steps: i32) -> u32 {
+fn run_simulation(
+    game_state: &mut GameState,
+    steps: i32,
+    messages: &mut Vec<String>,
+) -> (u32, Vec<String>) {
     if steps == 0 {
-        return 0;
+        return (0, vec![]);
     }
+
+    let mut best_steps: Vec<String> = vec![];
+    let mut best_step = format!("{} - init",steps);
+
+    // collect
 
     let mut max_score = 0;
-    // collect
-    {
-        for robot in robots.iter() {
-            match robot.produces {
-                Ore(amount) => { items.entry(Ore(1)).and_modify(|count| *count += amount); }
-                Clay(amount) => { items.entry(Clay(1)).and_modify(|count| *count += amount); }
-                Obsidian(amount) => { items.entry(Obsidian(1)).and_modify(|count| *count += amount); }
-                Geode(amount) => { items.entry(Geode(1)).and_modify(|count|
-                    *count += amount); }
-            }
-        }
-    }
-    // 0. build nothing
 
-    if steps>1 {
-        max_score = max(max_score, run_simulation(blueprint, items, robots, steps - 1));
-        // 1. try building ore
-        if is_material_sufficient(&blueprint.ore_robot, items) {
-            build_robot(&blueprint.ore_robot, items);
-            robots.push(Robot { produces: Ore(1) });
-            max_score = max(max_score, run_simulation(blueprint, items, robots, steps - 1));
-            robots.pop();
-            unbuild_robot(&blueprint.ore_robot, items);
+
+    // 4. try building geode
+    if steps > 1
+        && game_state.ores >= game_state.geode_ore_cost
+        && game_state.obsidians >= game_state.geode_obsidian_cost
+    {
+        game_state.ores -= game_state.geode_ore_cost;
+        game_state.obsidians -= game_state.geode_obsidian_cost;
+        game_state.collect_materials();
+        game_state.geode_robots += 1;
+        let (score, done_steps) = run_simulation(game_state, steps - 1, messages);
+        if score > max_score {
+            best_steps = done_steps;
+            best_step = format!("{}: G [{}]", steps, game_state);
+            max_score = score;
         }
+        game_state.geode_robots -= 1;
+        game_state.uncollect_materials();
+        game_state.ores += game_state.geode_ore_cost;
+        game_state.obsidians += game_state.geode_obsidian_cost;
+    } else {
+        if steps > 2 && game_state.obsidian_robots < game_state.max_obsidian_robots
+            && game_state.ores >= game_state.obsidian_ore_cost
+            && game_state.clays >= game_state.obsidian_clay_cost
+        {
+            game_state.ores -= game_state.obsidian_ore_cost;
+            game_state.clays -= game_state.obsidian_clay_cost;
+            game_state.collect_materials();
+            game_state.obsidian_robots += 1;
+            let (score, done_steps) = run_simulation(game_state, steps - 1, messages);
+            if score > max_score {
+                best_steps = done_steps;
+                best_step = format!("{}: O [{}]", steps, game_state);
+                max_score = score;
+            }
+            game_state.obsidian_robots -= 1;
+            game_state.uncollect_materials();
+            game_state.ores += game_state.obsidian_ore_cost;
+            game_state.clays += game_state.obsidian_clay_cost;
+        }
+
         // 2. try building clay
-        if is_material_sufficient(&blueprint.clay_robot, items) {
-            robots.push(Robot { produces: Clay(1) });
-            build_robot(&blueprint.clay_robot, items);
-            max_score = max(max_score, run_simulation(blueprint, items, robots, steps - 1));
-            unbuild_robot(&blueprint.clay_robot, items);
-            robots.pop();
-        }
-
-        // 3. try building obsidian
-        if is_material_sufficient(&blueprint.obsidian_robot, items) {
-            build_robot(&blueprint.obsidian_robot, items);
-            robots.push(Robot { produces: Obsidian(1) });
-            max_score = max(max_score, run_simulation(blueprint, items, robots, steps - 1));
-            unbuild_robot(&blueprint.obsidian_robot, items);
-            robots.pop();
-        }
-
-        // 4. try building geode
-        if is_material_sufficient(&blueprint.geode_robot, items) {
-            robots.push(Robot { produces: Geode(1) });
-            build_robot(&blueprint.geode_robot, items);
-            max_score = max(max_score, run_simulation(blueprint, items, robots, steps - 1));
-            unbuild_robot(&blueprint.geode_robot, items);
-            robots.pop();
-        }
-    }
-
-
-    let result = match items.get(&Geode(1)) {
-        Some(amount) => {
-            if *amount > max_score && steps>3 {
-                println!("new highscore {} /  {}", steps, *amount);
+        if steps > 3 && game_state.clay_robots < game_state.max_clay_robots
+            && game_state.ores >= game_state.clay_cost {
+            game_state.ores -= game_state.clay_cost;
+            game_state.collect_materials();
+            game_state.clay_robots += 1;
+            let (score, done_steps) = run_simulation(game_state, steps - 1, messages);
+            if score > max_score {
+                best_steps = done_steps;
+                best_step = format!("{}: C [{}]", steps, game_state);
+                max_score = score;
             }
-            max(max_score, *amount)
+            game_state.clay_robots -= 1;
+            game_state.uncollect_materials();
+            game_state.ores += game_state.clay_cost;
         }
-        None => 0
-    };
 
-    // uncollect
-    {
-        for robot in robots.iter() {
-            match robot.produces {
-                Ore(amount) => { items.entry(Ore(1)).and_modify(|count|
-                    *count -= amount); }
-                Clay(amount) => { items.entry(Clay(1)).and_modify(|count|
-                    *count -= amount); }
-                Obsidian(amount) => { items.entry(Obsidian(1)).and_modify(|count|
-                    *count -= amount); }
-                Geode(amount) => { items.entry(Geode(1)).and_modify(|count|
-                    *count -= amount); }
+        // 1. try building ore
+        if steps > 2 && game_state.ore_robots < game_state.max_ore_robots
+            && game_state.ores >= game_state.ore_cost {
+            game_state.ores -= game_state.ore_cost;
+            game_state.collect_materials();
+            game_state.ore_robots += 1;
+            let (score, done_steps) = run_simulation(game_state, steps - 1, messages);
+            if score > max_score {
+                best_steps = done_steps;
+                best_step = format!("{}: I [{}]", steps, game_state);
+                max_score = score;
+            }
+            game_state.ore_robots -= 1;
+            game_state.uncollect_materials();
+            game_state.ores += game_state.ore_cost;
+        }
+
+        if steps > 0 {
+            game_state.collect_materials();
+            let (score, done_steps) = run_simulation(game_state, steps - 1, messages);
+            game_state.uncollect_materials();
+            if score > max_score {
+                best_steps = done_steps;
+                best_step = format!("{}: - [{}]", steps, game_state);
+                max_score = score;
             }
         }
     }
 
-    result
+    best_steps.push(best_step);
+
+
+
+    (max(max_score, game_state.geodes), best_steps)
 }
 
-fn build_robot(blueprint: &Vec<Material>, items: &mut HashMap<Material, u32>) -> bool {
-    // use material and return robot
-    if is_material_sufficient(blueprint, items) {
-        for material in blueprint {
-            match material {
-                Ore(amount) => {
-                    items.entry(Ore(1)).and_modify(|a| *a -= amount);
-                }
-                Clay(amount) => {
-                    items.entry(Clay(1)).and_modify(|a| *a -= amount);
-                }
-                Obsidian(amount) => {
-                    items.entry(Obsidian(1)).and_modify(|a| *a -= amount);
-                }
-                Geode(amount) => {
-                    items.entry(Geode(1)).and_modify(|a| *a -= amount);
-                }
+pub fn run() {
+    let game_states = parse_input(read_file("input/day19-test.txt"));
+    let final_score = game_states
+        .par_iter()
+        .enumerate()
+        .map(|(id, game_state)| {
+            println!("Start Blueprint {}", id);
+            let mut messages = vec![];
+            let mut gs = game_state.clone();
+            let (score, done_steps) = run_simulation(&mut gs, 25, &mut messages);
+            println!("Score for Blueprint {}: {}", id, score);
+            for line in done_steps.iter().rev() {
+                println!("{}", line);
             }
-        }
-        return true;
-    }
-    false
-}
 
-fn is_material_sufficient(blueprint: &Vec<Material>, items: &mut HashMap<Material, u32>) -> bool {
-    for material in blueprint {
-        match material {
-            Ore(amount) => {
-                match items.get(&Ore(1)) {
-                    None => { return false; }
-                    Some(a) => {
-                        if a < amount {
-                            return false;
-                        }
-                    }
-                }
-            }
-            Clay(amount) => {
-                match items.get(&Clay(1)) {
-                    None => { return false; }
-                    Some(a) => {
-                        if a < amount {
-                            return false;
-                        }
-                    }
-                }
-            }
-            Obsidian(amount) => {
-                match items.get(&Obsidian(1)) {
-                    None => { return false; }
-                    Some(a) => {
-                        if a < amount {
-                            return false;
-                        }
-                    }
-                }
-            }
-            Geode(amount) => {
-                match items.get(&Geode(1)) {
-                    None => { return false; }
-                    Some(a) => {
-                        if a < amount {
-                            return false;
-                        }
-                    }
-                }
-            }
-        }
-    }
-    true
-}
 
-fn unbuild_robot(blueprint: &Vec<Material>, items: &mut HashMap<Material, u32>) {
-    for material in blueprint {
-        match material {
-            Ore(amount) => {
-                items.entry(Ore(1)).and_modify(|a| *a += amount);
-            }
-            Clay(amount) => {
-                items.entry(Clay(1)).and_modify(|a| *a += amount);
-            }
-            Obsidian(amount) => {
-                items.entry(Obsidian(1)).and_modify(|a| *a += amount);
-            }
-            Geode(amount) => {
-                items.entry(Geode(1)).and_modify(|a| *a += amount);
-            }
-        }
-    }
+            score * (id as u32 + 1)
+        })
+        .sum::<u32>();
+
+    println!("Final score: {}", final_score);
 }
 
 pub fn run2() {
@@ -253,9 +251,9 @@ pub fn run2() {
 
 #[cfg(test)]
 mod test {
+    use super::*;
     use crate::day19::Material::Obsidian;
     use crate::read_file;
-    use super::*;
 
     #[test]
     fn parses_input() {
@@ -267,4 +265,3 @@ mod test {
         assert_eq!(vec![Ore(2), Obsidian(7)], blueprints[0].geode_robot);
     }
 }
-
